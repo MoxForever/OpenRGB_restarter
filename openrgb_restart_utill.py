7c2f2a642661b14ccd0a40230b3e92f37e0c1316
MoxForever <opolskiji10@gmail.com>
2023-04-01 18:24:21 +0600

    requirements.txt added forbid start several instances

--- openrgb_restart_utill.py
+++ openrgb_restart_utill.py
@@ -3,8 +3,10 @@ import time
 
 import psutil
 import usb
+import tendo.singleton
 
 
+# get all connected devices
 def get_device_pids():
     data = []
     for bus in usb.busses():
@@ -12,10 +14,12 @@ def get_device_pids():
     return data
 
 
+# get diff between 2 arrays
 def get_difference(a: list[int], b: list[int]):
     return list(set(a).symmetric_difference(set(b)))
 
 
+# stop and start new instance of OpenRGB
 def restart_open_rgb():
     for proc in psutil.process_iter():
         if proc.name() == "OpenRGB.exe":
@@ -24,17 +28,24 @@ def restart_open_rgb():
     os.startfile('OpenRGB.exe')
 
 
-last_pids = None
-while True:
-    new_pids = get_device_pids()
-    if last_pids is None:
-        last_pids = new_pids
-        continue
+try:
+    # for one working instance only
+    me = tendo.singleton.SingleInstance()
+except tendo.singleton.SingleInstanceException:
+    print("One instance is running already")
+else:
+    # loop for checking new devices
+    last_pids = None
+    while True:
+        new_pids = get_device_pids()
+        if last_pids is None:
+            last_pids = new_pids
+            continue
 
-    diff = get_difference(last_pids, new_pids)
-    if len(diff) > 0:
-        restart_open_rgb()
-        print("ready!")
-        last_pids = new_pids
+        diff = get_difference(last_pids, new_pids)
+        if len(diff) > 0:
+            restart_open_rgb()
+            print("ready!")
+            last_pids = new_pids
 
-    time.sleep(1)
+        time.sleep(1)
